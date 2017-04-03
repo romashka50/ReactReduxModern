6fa5986653295abe770228b5d55c8d51ae923fd4
romashka50 <roma.buchuk@gmail.com>
2017-04-03 22:27:17 +0300

    temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyArVDPloh4qmPyagaPiSat1PzOhAYhHr9E';
+
 const App = () => {
   return <div>
-        Hello world
-    </div>;
+    Hello world
+  </div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
