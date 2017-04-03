c776c12a4e4cdc20e472cdc578a9aa354857d290
romashka50 <roma.buchuk@gmail.com>
2017-04-03 23:02:29 +0300

    add videi title & video image

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const VideoItem = (props) => (
-  <li>
-    Video
-  </li>
-);
+const VideoItem = ({ video }) => {
+  const {
+    snippet: {
+      thumbnails: {
+        default: { url }
+      },
+      title,
+    }
+  } = video;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img alt="" className="media-object" src={url}/>
+        </div>
+      </div>
+
+      <div className="media-body">
+        <div className="media-heading">
+          {title}
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default VideoItem;
