ba3a644f288bcb1531c8afbe2393919b8c31e6de
romashka50 <roma.buchuk@gmail.com>
2017-04-16 20:54:28 +0300

    search-bar state

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '6a922516b164af8d3be6994aa24c99cc';
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    console.log(e.target.value);
+    this.setState({ term: e.target.value });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form
+        className="input-group"
+        onSubmit={this.onFormSubmit}
+      >
+        <input
+          className="form-control"
+          placeholder="Search for weather"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
