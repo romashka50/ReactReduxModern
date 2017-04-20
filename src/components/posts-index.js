82e80f1d6cef294f52f10336f539d946c4838be1
romashka50 <roma.buchuk@gmail.com>
2017-04-20 22:29:24 +0300

    add fetchPosts

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default props =>
-  // const { p1, p2 } = props;
-   (
-     <div >
-      List of Posts
-    </div>
-  );
+class PostIndex extends Component {
+
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    // const { p1, p2 } = this.props;
+    return (
+      <div className="" >
+        List of posts
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchPosts })(PostIndex);
