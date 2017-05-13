cea64553892bd6de79a9f662fa7f5fcb2646703a
romashka50 <roma.buchuk@gmail.com>
2017-05-13 11:06:34 +0300

    finish section 3; wrote test-helper.js

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,40 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import jquery from 'jquery';
 import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
+import TestUtis from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
 import reducers from '../src/reducers';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+const { JSDOM } = jsdom;
 
-chaiJquery(chai, chai.util, $);
+global.window = new JSDOM('<!doctype html><html><body></body></html>').window;
+global.document = global.window.document;
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+const $ = jquery(global.window);
+
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtis.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
-    </Provider>
+    </Provider >,
   );
 
+  // eslint-disable-next-line
   return $(ReactDOM.findDOMNode(componentInstance));
 }
 
-$.fn.simulate = function(eventName, value) {
+$.fn.simulate = function (eventName, value) {
   if (value) {
     this.val(value);
   }
-  TestUtils.Simulate[eventName](this[0]);
+
+  TestUtis.Simulate[eventName](this[0]);
 };
 
-export {renderComponent, expect};
+chaiJquery(chai, chai.util, $);
+
+export { expect, renderComponent };
