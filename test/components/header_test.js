c5f6a241e63d2bd0cddc925942dbd0e436b4d2e1
romashka50 <roma.buchuk@gmail.com>
2017-05-13 21:58:49 +0300

    add header_test

--- test/components/header_test.js
+++ test/components/header_test.js
@@ -3,5 +3,17 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/header';
 
 describe('CommentList', () => {
+  let component;
 
+  beforeEach(()=>{
+    component = renderComponent(CommentBox);
+  });
+
+  it('shows an Li for each menu', () => {
+    expect(component.find('li').length).to.be.equal(3);
+  });
+
+  it('shows each li with class nav-item', () => {
+    expect(component.find('li')).to.have.class('nav-item');
+  });
 });
