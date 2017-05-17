6d8b2a7891f350c3b8ea921bf2102fa0dbe6c5cd
romashka50 <roma.buchuk@gmail.com>
2017-05-17 21:19:46 +0300

    add user-list container

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import UserList from './user_list';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-       Content
+        <UserList />
       </div>
     );
   }
---
+++ src/components/user_list.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
+class UserList extends Component {
+  componentWillMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUser(user) {
+    return (
+      <div className="card card-block">
+        <h4 className="card-title">{user.name}</h4>
+        <p className="card-text">Simple Factory</p>
+        <a className="btn btn-primary">Email</a>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="" >
+        {this.props.users.map(this.renderUser)}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ users }) {
+  return { users };
+}
+
+export default connect(mapStateToProps, actions)(UserList);
